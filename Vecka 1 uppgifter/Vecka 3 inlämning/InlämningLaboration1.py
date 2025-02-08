ad2b1491e8742668cf81becd3460b4b987605cbc
Davinchevo <Djohanssn@hotmail.com>
2025-02-08 20:02:05 +0100

    Add function to check guesses in Hangman game and manage lives

--- Vecka 1 uppgifter/Vecka 3 inlämning/InlämningLaboration1.py
+++ Vecka 1 uppgifter/Vecka 3 inlämning/InlämningLaboration1.py
@@ -1,10 +1,17 @@
-
+def check_guess(guess_char):
+    lifes = 6
+    while lifes > 0:
+        for i in range(len(list_of_characters)):
+            if guess_char == list_of_characters[i]:
+                dict_of_stars[i] = guess_char
+            else:
+                lifes -= 1
+                print("Wrong guess, you have", lifes, "lifes left")
 
 
 hangman = input("Enter a word: ")
 list_of_characters = list(hangman)
 
-# Create a dictionary with "*" as the key and the value as a string of asterisks of the same length as the word
 dict_of_stars = {"*" * len(hangman) }
 
 print(dict_of_stars)
