a0fb1c29f01632ac0db4930f7d4c1abe9f89ddbe
Davinchevo <Djohanssn@hotmail.com>
2025-02-09 11:40:44 +0100

    Refactor Hangman game logic to improve guess handling and track incorrect guesses

--- Vecka 1 uppgifter/Vecka 3 inlämning/InlämningLaboration1.py
+++ Vecka 1 uppgifter/Vecka 3 inlämning/InlämningLaboration1.py
@@ -1,17 +1,37 @@
-def check_guess(guess_char):
+def hangman():
     lifes = 6
+    incorrect_guesses = ""
     while lifes > 0:
+        input_char = input("Enter a character: ")
+        if len(input_char) != 1:
+            print("You can only enter one character at a time, try again")
+            continue
+        if input_char in incorrect_guesses:
+           print("You have already guessed that character, try again")
+           continue
+        guessed = False
         for i in range(len(list_of_characters)):
-            if guess_char == list_of_characters[i]:
-                dict_of_stars[i] = guess_char
-            else:
-                lifes -= 1
-                print("Wrong guess, you have", lifes, "lifes left")
+            if input_char == list_of_characters[i]:
+                dict_of_stars["*"] = dict_of_stars["*"][:i] + input_char + dict_of_stars["*"][i+1:]
+                guessed = True
+
+        if guessed:
+            print("Correct guess!")
+        else:
+            lifes -= 1
+            incorrect_guesses += input_char
+            print("Wrong guess, you have", lifes, "lives left")
+            print("Incorrect guesses:", incorrect_guesses)
+
+        print(dict_of_stars["*"])
+
+        if "*" not in dict_of_stars["*"]:
+            print("You won! The word was:", dict_of_stars["*"])
+            break
 
 
-hangman = input("Enter a word: ")
-list_of_characters = list(hangman)
+hangman_word = input("Enter a word: ")
+list_of_characters = list(hangman_word)
+dict_of_stars = {"*": "*" * len(hangman_word)}  
 
-dict_of_stars = {"*" * len(hangman) }
-
-print(dict_of_stars)
+hangman()
